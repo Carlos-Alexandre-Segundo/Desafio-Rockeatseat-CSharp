b007b286cb603ec449857d4f8a44c6eb2e571376
Carlos Segundo <segundocarlosalexandre@gmail.com>
2026-02-06 13:10:55 -0400

    Adicionar arquivos de projeto.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using Helloworld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld;
+class Program
+{
+    enum nivelDeDificuldade 
+    { 
+        Facil,
+        Medio,
+        Dificil,
+        MuitoDificil
+    }
+
+    static void Main()
+    {
+        nivelDeDificuldade nivel = nivelDeDificuldade.Dificil;
+
+        Console.WriteLine($"O nível de dificuldade selecionado é: {nivel}");
+    }
+
+}
---
+++ HelloWorld/carro.cs
@@ -0,0 +1,24 @@
+﻿namespace Helloworld;
+
+class Carro
+{
+    public void Ligar()
+    {
+        Console.WriteLine("Carro Ligado");
+    }
+
+    public void Desligar() 
+    {
+        Console.WriteLine("Carro Desligado");
+    }
+
+    private void Teste1()
+    {
+        Console.WriteLine("Teste 1");
+    }
+
+    internal void Teste2()
+    {
+        Console.WriteLine("Carro Desligado");
+    }
+}
